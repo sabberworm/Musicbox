83fe44185fba1054305c100a82d50533124bff6b
Raphael Schweikert <any@sabberworm.com>
2011-08-09 18:26:46 +0200

    Pushed logging from console.log to custom log function (uses ansi-color if available).

--- musicbox.js
+++ musicbox.js
@@ -1,17 +1,19 @@
 var fs = require('fs'),
-    sys = require('sys'),
-    yaml = require('yaml');
+		sys = require('sys'),
+		yaml = require('yaml')
+		log = require('./lib/log.js');
 
 fs.readFile('./config/config.yaml', function(err, data) {
 	var settings = {};
 	if (err) {
-		sys.puts('No settings.json found ('+err+'). Aborting');
+		log('error', 'No settings.json found ('+err+'). Aborting');
+		process.exit(1);
 	} else {
 		try {
 			settings = yaml.eval(data.toString('utf8',0,data.length));
-			console.log("Starting Musicbox with settings ", settings);
+			log('info', "Starting Musicbox with settings ", settings);
 		} catch (e) {
-			sys.puts('Error parsing config.yaml: '+e);
+			log('error', 'Error parsing config.yaml: '+e);
 			process.exit(1);
 		}
 		var bootstrap = require('./lib/bootstrap.js');
